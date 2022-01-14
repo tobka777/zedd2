17d20cba66e28761718198055376d7fdf94644f6
anbespal <andrej.bespalov@capgemini.com>
2022-01-14 11:00:37 +0100

    merge jira.js

--- zedd-app/src/main.ts
+++ zedd-app/src/main.ts
@@ -1,4 +1,4 @@
-import { app, ipcMain, session, BrowserWindow } from 'electron'
+import { app, ipcMain, /* session,*/ BrowserWindow } from 'electron'
 
 global.isDev = process.argv.includes('--dev')
 
@@ -6,10 +6,12 @@ global.appUserModelId = global.isDev ? process.execPath : 'com.squirrel.zedd.zed
 app.setAppUserModelId(global.appUserModelId)
 app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors')
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1'
+/*
 session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
   details.requestHeaders['User-Agent'] = 'zedd-app';
   callback({ cancel: false, requestHeaders: details.requestHeaders });
 });
+*/
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
